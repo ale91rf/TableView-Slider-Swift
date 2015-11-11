fffbc9e3b8e2a23bcdfaf7f20baeb001e9eb2741
Ale Fernandez <alex.rf.46@gmail.com>
2015-11-11 17:06:06 +0100

    mini App with TableView and Slider implemented

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var mTable: UITableView!
+    
+    @IBOutlet weak var mSliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        mTable.reloadData()
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let mTimesTable = Int(mSliderValue.value * 20)
+        
+        cell.textLabel?.text = String(mTimesTable * (indexPath.row + 1))
+        
+        return cell
+        
     }
 
 
